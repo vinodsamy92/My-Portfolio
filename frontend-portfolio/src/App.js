f9cf591495d250cd9894a4ae68afe959eaca00cc
vinodsamy92 <vinodsamymf@gmail.com>
2022-09-01 18:50:13 +0530

    +images and navbar style

--- frontend-portfolio/src/App.js
+++ frontend-portfolio/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import { About, Header, Footer, Skills, Testimonial, Work } from "./container/index"
 import { Navbar } from "./components/index"
+import "./App.scss"
 export default function App() {
   return (
     <div className="app">
